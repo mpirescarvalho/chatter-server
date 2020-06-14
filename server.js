784f0480ae637d8e7c5c645a008363d4867d2645
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-06-14 19:09:18 -0300

    create room logic

--- server.js
+++ server.js
@@ -25,115 +25,41 @@ const io = require("socket.io")(server);
 // 	}[];
 // }
 
-let rooms = [
-	{
-		id: "1",
-		name: "Room 1",
-		people: [
-			{
-				id: "1",
-				nickname: "Marcelo",
-			},
-			{
-				id: "2",
-				nickname: "João",
-			},
-		],
-	},
-	{
-		id: "2",
-		name: "Room 2",
-		people: [
-			{
-				id: "3",
-				nickname: "Marcos",
-			},
-		],
-	},
-];
+// let rooms = [
+// 	{
+// 		id: "1",
+// 		name: "Room 1",
+// 		people: [
+// 			{
+// 				id: "1",
+// 				nickname: "Marcelo",
+// 			},
+// 			{
+// 				id: "2",
+// 				nickname: "João",
+// 			},
+// 		],
+// 	},
+// 	{
+// 		id: "2",
+// 		name: "Room 2",
+// 		people: [
+// 			{
+// 				id: "3",
+// 				nickname: "Marcos",
+// 			},
+// 		],
+// 	},
+// ];
 
-// let rooms = [];
-
-function handleExitRoom(id) {
-	for (let j = 0; j < rooms.length; j++) {
-		const f_room = rooms[j];
-		for (let i = 0; i < f_room.people.length; i++) {
-			const f_people = f_room.people[i];
-			if (f_people.id === id) {
-				f_room.people.splice(i, 1);
-				if (f_room.people.length <= 0) {
-					rooms.slice(i, 1);
-				} else {
-					io.emit(`${f_room.id}-changed`, f_room);
-				}
-				io.emit("rooms_changed", rooms);
-				return;
-			}
-		}
-	}
-}
+let rooms = [];
 
 io.on("connection", (socket) => {
-	//Rooms changed event
-	function roomsChanged() {
-		socket.broadcast.emit("rooms_changed", rooms);
-	}
-
-	//Destroy room if there's no one inside it
-	// const checkRoomDestroy = () => {
-	// 	for (let i = rooms.length - 1; i >= 0; i--) {
-	// 		const f_room = rooms[i];
-	// 		if (f_room.people.length <= 0) {
-	// 			rooms.slice(i, 1);
-	// 			roomsChanged();
-	// 		}
-	// 	}
-	// };
-
-	//Create room event
-	// socket.on("create_room", (name) => {
-	// 	const hash = crypto.randomBytes(6).toString("hex");
-	// 	const room = { id: hash, name, people: [] };
-	// 	rooms.push(room);
-	// 	roomsChanged();
-	// 	socket.emit("room_cheated", room);
-	// 	assignRoomEvents(room);
-	// });
-
-	// const assignRoomEvents = (room) => {
-	// console.log(`assigning events for room ${room.id}`);
-
-	//Enter room event
-	// socket.on(`${room.id}-enter_room`, (nickname) => {
-	// 	rooms.forEach((f_room) => {
-	// 		if (f_room.id === room.id) {
-	// 			console.log(`${nickname} entering room ${room.id}`);
-	// 			f_room.people.push({ id: socket.id, nickname });
-	// 			connected_to_room = f_room.id;
-	// 			socket.emit(`${room.id}-info`, room);
-	// 			roomsChanged();
-	// 		}
-	// 	});
-	// });
-
-	//Exit room event
-	// socket.on(`${room.id}-exit_room`, () => handleExitRoom(room.id));
-
-	//Send message to room event
-	// socket.on(`${room.id}-send_message`, (message) => {
-	// 	socket.broadcast.emit(`${room.id}-new_message`, {
-	// 		sender_id: socket.id,
-	// 		message,
-	// 	});
-	// });
-	// };
-
-	// rooms.forEach(assignRoomEvents);
-
 	//socket disconected
 	socket.on("disconnect", () => handleExitRoom(socket.id));
 });
 
+//get specific room indo
 app.get("/rooms/:id", (req, res) => {
 	const { id } = req.params;
 	const room = rooms.find((room) => room.id === id);
@@ -141,13 +67,14 @@ app.get("/rooms/:id", (req, res) => {
 	else res.status(404).json({ message: "room not found" });
 });
 
+//get rooms info
 app.get("/rooms", (req, res) => res.json(rooms));
 
+//enter room
 app.post("/rooms/people", (req, res) => {
 	const { id, room_id, nickname } = req.body;
 	const _room = rooms.find((room) => room.id === room_id);
 	if (_room) {
-		console.log(`${nickname} entering room ${room_id}`);
 		_room.people.push({ id, nickname });
 		res.json(_room);
 		io.emit("rooms_changed", rooms);
@@ -158,6 +85,7 @@ app.post("/rooms/people", (req, res) => {
 	}
 });
 
+//send message
 app.post("/messages", (req, res) => {
 	const { message, sender_id, room_id } = req.body;
 	if (message && sender_id && room_id) {
@@ -167,5 +95,38 @@ app.post("/messages", (req, res) => {
 		res.status(400).json({ message: "fail" });
 	}
 });
+
+//create room
+app.post("/rooms", (req, res) => {
+	const { name } = req.body;
+	if (name) {
+		const hash = crypto.randomBytes(6).toString("hex");
+		const room = { id: hash, name, people: [] };
+		rooms.push(room);
+		io.emit("rooms_changed", rooms);
+		res.json({ room_id: hash });
+	} else {
+		res.status(400).json({ message: "fail" });
+	}
+});
+
+function handleExitRoom(id) {
+	for (let j = 0; j < rooms.length; j++) {
+		const f_room = rooms[j];
+		for (let i = 0; i < f_room.people.length; i++) {
+			const f_people = f_room.people[i];
+			if (f_people.id === id) {
+				f_room.people.splice(i, 1);
+				if (f_room.people.length <= 0) {
+					rooms.splice(j, 1);
+				} else {
+					io.emit(`${f_room.id}-changed`, f_room);
+				}
+				io.emit("rooms_changed", rooms);
+				return;
+			}
+		}
+	}
+}
 
 server.listen(4001);
