7e4daf0414d833a5710944653de17a307a59faf4
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2022-01-06 10:56:36 -0300

    fix cors issue

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.json());
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
+io.set('origins', 'https://chatter-room.netlify.app');
+
 // let messages: {
 // 	room_id: string,
 // 	sender_id: string,
